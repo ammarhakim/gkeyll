edb6dc1be38110916135f35dfa750b983fdfc3e3
Junoravin <junoravin@gmail.com>
2025-08-06 17:46:10 -0700

    Also need to the do the same for the 3v two stream

--- vlasov/luareg/rt_vlasov_sr_twostream_1x3v.lua
+++ vlasov/luareg/rt_vlasov_sr_twostream_1x3v.lua
@@ -145,7 +145,7 @@ vlasovApp = Vlasov.App.new {
     init = function (t, xn)
       local x = xn[1]
 
-      local Ex = -alpha * math.sin(kx * x) / kx -- Total electric field (x-direction).
+      local Ex = -alpha * gamma_elc1 * math.sin(kx * x) / kx -- Total electric field (x-direction).
       local Ey = 0.0 -- Total electric field (y-direction).
       local Ez = 0.0 -- Total electric field (z-direction).
 
