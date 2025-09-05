43b00151cd8ce99fe60b29132ac337c45eca31bc
johnson452 <grj@princeton.edu>
2025-09-05 15:41:14 -0400

    Fix to nodal to modal math for triad geom objects

--- vlasov/ker/vlasov/vlasov_nc_hamil_gen_vol_1x1v_ser_p2.c
+++ vlasov/ker/vlasov/vlasov_nc_hamil_gen_vol_1x1v_ser_p2.c
@@ -14,21 +14,4 @@ GKYL_CU_DH void vlasov_nc_hamil_gen_vol_1x1v_ser_p2(const double *w, const doubl
   out[6] += (6.0*hamil[2]*poisson_tensor_conf_0[2]*f[7]+6.708203932499369*poisson_tensor_conf_0[0]*hamil[2]*f[7]+3.0*hamil[1]*poisson_tensor_conf_0[1]*f[6]+6.708203932499369*poisson_tensor_conf_0[1]*hamil[2]*f[5]+6.708203932499369*poisson_tensor_conf_0[1]*hamil[2]*f[4]+3.0*hamil[1]*poisson_tensor_conf_0[2]*f[3]+3.354101966249684*poisson_tensor_conf_0[0]*hamil[1]*f[3]+6.708203932499369*f[1]*hamil[2]*poisson_tensor_conf_0[2]+7.500000000000001*f[0]*poisson_tensor_conf_0[1]*hamil[2]+7.500000000000001*poisson_tensor_conf_0[0]*f[1]*hamil[2]+3.354101966249684*hamil[1]*poisson_tensor_conf_0[1]*f[2])*dv10*dx10; 
   out[7] += (1.5*hamil[1]*poisson_tensor_conf_0[1]*f[7]+3.0*hamil[2]*poisson_tensor_conf_0[2]*f[6]+1.5*poisson_tensor_conf_0[0]*hamil[1]*f[5]+3.0*poisson_tensor_conf_0[1]*hamil[2]*f[3]+3.0*poisson_tensor_conf_0[0]*f[2]*hamil[2])*dv10*dx10; 
 
-printf(
-  "[VOL DBG] dxv[0]=%.17g dxv[1]=%.17g | dx10=%.17g dv10=%.17g dv10_sq=%.17g | "
-  "w[1]=%.17g | hamil[1]=%.17g hamil[2]=%.17g | "
-  "f[0..7]=[%.17g, %.17g, %.17g, %.17g, %.17g, %.17g, %.17g, %.17g] | "
-  "poisson_tensor_conf_0=%p -> {%.17g, %.17g, %.17g} | "
-  "factor(dv10*dx10)=%.17g | out[1]=%.17g out[3]=%.17g out[4]=%.17g out[6]=%.17g out[7]=%.17g\n",
-  dxv[0], dxv[1],
-  dx10, dv10, dv10_sq,
-  w[1],
-  hamil[1], hamil[2],
-  f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7],
-  (void*)poisson_tensor_conf_0,
-  poisson_tensor_conf_0[0], poisson_tensor_conf_0[1], poisson_tensor_conf_0[2],
-  dv10*dx10,
-  out[1], out[3], out[4], out[6], out[7]
-);
-
 } 
--- vlasov/zero/vlasov_triad_geom.c
+++ vlasov/zero/vlasov_triad_geom.c
@@ -11,6 +11,22 @@
 #include <gkyl_vlasov_triad_geom_priv.h>
 
 
+static inline void
+log_to_comp(int ndim, const double *eta,
+  const double * GKYL_RESTRICT dx, const double * GKYL_RESTRICT xc,
+  double* GKYL_RESTRICT xout)
+{
+  for (int d=0; d<ndim; ++d) xout[d] = 0.5*dx[d]*eta[d]+xc[d];
+}
+
+// Identity comp to phys coord mapping, for when user doesn't provide a map.
+static inline void
+c2p_identity(const double *xcomp, double *xphys, int ndim)
+{
+  for (int d=0; d<ndim; d++) xphys[d] = xcomp[d];
+}
+
+
 void
 gkyl_vlasov_triad_geom_new(const struct gkyl_rect_grid *cgrid, const struct gkyl_range *crange, const struct gkyl_basis cbasis, 
   const struct gkyl_rect_grid *pgrid, const struct gkyl_range *prange, const struct gkyl_basis pbasis, 
@@ -23,43 +39,6 @@ gkyl_vlasov_triad_geom_new(const struct gkyl_rect_grid *cgrid, const struct gkyl
   int cdim = cgrid->ndim;
   int pdim = pgrid->ndim;
   int vdim = pdim - cdim;
-  struct gkyl_array *cov_tangent_basis_nodal;
-  struct gkyl_array *triad_basis_nodal;
-  struct gkyl_array *triad_basis_gradient_nodal;
-  struct gkyl_array *h_ij_nodal;
-  struct gkyl_array *h_ij_inv_nodal;
-  struct gkyl_array *det_h_nodal;
-  struct gkyl_array *conf_poisson_tensor_nodal;
-
-  // Configuration space nodal grid construction from cgrid
-  const double *clower = cgrid->lower, *cupper = cgrid->upper;
-  const int *cells = cgrid->cells;
-
-  // Nodal grid used for constructing physical coordinates in configuration space
-  double nc_lower[GKYL_MAX_CDIM];
-  double nc_upper[GKYL_MAX_CDIM];
-  int nc_cells[GKYL_MAX_CDIM];
-  int nghost[GKYL_MAX_CDIM];
-  for (int i=0; i<cdim; ++i) {
-    nc_lower[i] = clower[i] - 0.5*cgrid->dx[i];
-    nc_upper[i] = cupper[i] + 0.5*cgrid->dx[i];
-    nc_cells[i] = 2*cells[i] + 1; 
-    nghost[i] = 1; // One ghost cell in conf space
-  }
-
-  struct gkyl_rect_grid nc_grid;
-  struct gkyl_range nc_local, nc_local_ext;
-  gkyl_rect_grid_init(&nc_grid, cdim, nc_lower, nc_upper, nc_cells);
-  gkyl_create_grid_ranges(&nc_grid, nghost, &nc_local_ext, &nc_local);
-
-  cov_tangent_basis_nodal = gkyl_array_new(GKYL_DOUBLE, vdim*vdim, nc_local.volume);
-  triad_basis_nodal = gkyl_array_new(GKYL_DOUBLE, vdim*vdim, nc_local.volume);
-  h_ij_nodal = gkyl_array_new(GKYL_DOUBLE, vdim*(vdim+1)/2, nc_local.volume);
-  h_ij_inv_nodal = gkyl_array_new(GKYL_DOUBLE, vdim*(vdim+1)/2, nc_local.volume);
-  det_h_nodal = gkyl_array_new(GKYL_DOUBLE, 1, nc_local.volume);
-  conf_poisson_tensor_nodal = gkyl_array_new(GKYL_DOUBLE, num_pt_indices[vdim-1], nc_local.volume);
-  triad_basis_gradient_nodal = gkyl_array_new(GKYL_DOUBLE, vdim*vdim*vdim, nc_local.volume);
-  double xn[GKYL_MAX_DIM];
 
   // Choose functions
   metric_t compute_h_ij_from_cov_tangent_basis = choose_metric_kern(vdim); 
@@ -67,40 +46,13 @@ gkyl_vlasov_triad_geom_new(const struct gkyl_rect_grid *cgrid, const struct gkyl
   metric_det_t compute_det_h_ij = choose_metric_det_kern(vdim); 
   conf_poisson_tensor_t compute_triad_poisson_tensor_ij = choose_conf_poisson_tensor_kern(vdim); 
 
-  // initialize nodal values in configuration space
-  struct gkyl_range_iter iter;
-  gkyl_range_iter_init(&iter, &nc_local);
-  while (gkyl_range_iter_next(&iter)) {
-    long nidx = gkyl_range_idx(&nc_local, iter.idx);
-    
-    gkyl_rect_grid_ll_node(cgrid, iter.idx, xn);
-
-    double *pn_cov_tangent_basis = gkyl_array_fetch(cov_tangent_basis_nodal, nidx);
-    double *pn_triad_basis = gkyl_array_fetch(triad_basis_nodal, nidx);
-    double *pn_h_ij = gkyl_array_fetch(h_ij_nodal, nidx);
-    double *pn_h_ij_inv = gkyl_array_fetch(h_ij_inv_nodal, nidx);
-    double *pn_det_h = gkyl_array_fetch(det_h_nodal, nidx);
-    double *pn_conf_poisson_tensor = gkyl_array_fetch(conf_poisson_tensor_nodal, nidx);
-    double *pn_triad_basis_gradient = gkyl_array_fetch(triad_basis_gradient_nodal, nidx);
-
-    // Evaluate the functions for basis and their gradients at a nodal point xn
-    inp_basis_vectors.eval_cov_tangent_basis(0.0, xn, pn_cov_tangent_basis, inp_basis_vectors.ctx);
-    inp_basis_vectors.eval_triad_basis(0.0, xn, pn_triad_basis, inp_basis_vectors.ctx);
-    inp_basis_vectors.eval_triad_basis_gradient(0.0, xn, pn_triad_basis_gradient, inp_basis_vectors.ctx);
-
-    // Compute the metric at nodal points
-    compute_h_ij_from_cov_tangent_basis(pn_cov_tangent_basis, pn_h_ij);
-
-    // Compute the metric inverse at nodal points
-    compute_h_ij_inv_from_h_ij(pn_h_ij, pn_h_ij_inv);
-
-    // Compute the sqrt(det(h_ij)) at nodal points
-    compute_det_h_ij(pn_h_ij, pn_det_h);
-
-    // Compute the Poisson Tensor in configruation space components at nodal points
-    compute_triad_poisson_tensor_ij(pn_h_ij_inv, pn_triad_basis, pn_cov_tangent_basis,
-      pn_triad_basis_gradient, pn_conf_poisson_tensor);
-  }
+  struct gkyl_array *cov_tangent_basis_at_nodes;
+  struct gkyl_array *triad_basis_at_nodes;
+  struct gkyl_array *triad_basis_gradient_at_nodes;
+  struct gkyl_array *h_ij_at_nodes;
+  struct gkyl_array *h_ij_inv_at_nodes;
+  struct gkyl_array *det_h_at_nodes;
+  struct gkyl_array *conf_poisson_tensor_at_nodes;
 
   // Convert nodal to modal using the computed nodal quantities
   gkyl_eval_on_nodes *h_ij_proj;
@@ -113,14 +65,72 @@ gkyl_vlasov_triad_geom_new(const struct gkyl_rect_grid *cgrid, const struct gkyl
   det_h_proj = gkyl_eval_on_nodes_new(cgrid, &cbasis, 1, NULL, NULL);
   conf_poisson_tensor_proj = gkyl_eval_on_nodes_new(cgrid, &cbasis, num_pt_indices[vdim-1], NULL, NULL);
 
+
+  double xc[GKYL_MAX_DIM], xmu[GKYL_MAX_DIM];
+  int num_basis = cbasis.num_basis;
+
+  // Eval functions
+  cov_tangent_basis_at_nodes = gkyl_array_new(GKYL_DOUBLE, vdim*vdim, num_basis);
+  triad_basis_at_nodes = gkyl_array_new(GKYL_DOUBLE, vdim*vdim, num_basis);
+  triad_basis_gradient_at_nodes = gkyl_array_new(GKYL_DOUBLE, vdim*vdim*vdim, num_basis);
+
+  // Derived quantities from eval functions
+  h_ij_at_nodes = gkyl_array_new(GKYL_DOUBLE, vdim*(vdim+1)/2, num_basis);
+  h_ij_inv_at_nodes = gkyl_array_new(GKYL_DOUBLE, vdim*(vdim+1)/2, num_basis);
+  det_h_at_nodes = gkyl_array_new(GKYL_DOUBLE, 1, num_basis);
+  conf_poisson_tensor_at_nodes = gkyl_array_new(GKYL_DOUBLE, num_pt_indices[vdim-1], num_basis);
+
+  struct gkyl_range_iter iter;
   gkyl_range_iter_init(&iter, crange);
+  
   while (gkyl_range_iter_next(&iter)) {
+    gkyl_rect_grid_cell_center(cgrid, iter.idx, xc);
+
+    for (int i=0; i<num_basis; ++i) {
+
+      // Grab the local node list
+      struct gkyl_array *nodes = gkyl_array_new(GKYL_DOUBLE, cgrid->ndim, cbasis.num_basis);
+      cbasis.node_list(gkyl_array_fetch(nodes, 0));
+      log_to_comp(cgrid->ndim, gkyl_array_cfetch(nodes, i),
+        cgrid->dx, xc, xmu);
+      c2p_identity(xmu, xmu, cgrid->ndim);
+
+      // Evaluate the functions for basis and their gradients at a nodal point xmu
+      inp_basis_vectors.eval_cov_tangent_basis(0.0, xmu, gkyl_array_fetch(cov_tangent_basis_at_nodes, i), inp_basis_vectors.ctx);
+      inp_basis_vectors.eval_triad_basis(0.0, xmu, gkyl_array_fetch(triad_basis_at_nodes, i), inp_basis_vectors.ctx);
+      inp_basis_vectors.eval_triad_basis_gradient(0.0, xmu, gkyl_array_fetch(triad_basis_gradient_at_nodes, i), inp_basis_vectors.ctx);
+
+      // Fill the remaining nodal quantities:
+      double *pn_cov_tangent_basis = gkyl_array_fetch(cov_tangent_basis_at_nodes, i);
+      double *pn_triad_basis = gkyl_array_fetch(triad_basis_at_nodes, i);
+      double *pn_h_ij = gkyl_array_fetch(h_ij_at_nodes, i);
+      double *pn_h_ij_inv = gkyl_array_fetch(h_ij_inv_at_nodes, i);
+      double *pn_det_h = gkyl_array_fetch(det_h_at_nodes, i);
+      double *pn_conf_poisson_tensor = gkyl_array_fetch(conf_poisson_tensor_at_nodes, i);
+      double *pn_triad_basis_gradient = gkyl_array_fetch(triad_basis_gradient_at_nodes, i);
+
+      // Compute the metric at nodal points
+      compute_h_ij_from_cov_tangent_basis(pn_cov_tangent_basis, pn_h_ij);
+
+      // Compute the metric inverse at nodal points
+      compute_h_ij_inv_from_h_ij(pn_h_ij, pn_h_ij_inv);
+
+      // Compute the sqrt(det(h_ij)) at nodal points
+      compute_det_h_ij(pn_h_ij, pn_det_h);
+
+      // Compute the Poisson Tensor in configruation space components at nodal points
+      compute_triad_poisson_tensor_ij(pn_h_ij_inv, pn_triad_basis, pn_cov_tangent_basis,
+        pn_triad_basis_gradient, pn_conf_poisson_tensor);
+        
+    }
+
     long lidx = gkyl_range_idx(crange, iter.idx);
-    gkyl_eval_on_nodes_nod2mod(h_ij_proj, h_ij_nodal, gkyl_array_fetch(h_ij, lidx));
-    gkyl_eval_on_nodes_nod2mod(h_ij_inv_proj, h_ij_inv_nodal, gkyl_array_fetch(h_ij_inv, lidx));
-    gkyl_eval_on_nodes_nod2mod(det_h_proj, det_h_nodal, gkyl_array_fetch(det_h, lidx));
-    gkyl_eval_on_nodes_nod2mod(conf_poisson_tensor_proj, conf_poisson_tensor_nodal, gkyl_array_fetch(conf_poisson_tensor, lidx));
+    gkyl_eval_on_nodes_nod2mod(h_ij_proj, h_ij_at_nodes, gkyl_array_fetch(h_ij, lidx));
+    gkyl_eval_on_nodes_nod2mod(h_ij_inv_proj, h_ij_inv_at_nodes, gkyl_array_fetch(h_ij_inv, lidx));
+    gkyl_eval_on_nodes_nod2mod(det_h_proj, det_h_at_nodes, gkyl_array_fetch(det_h, lidx));
+    gkyl_eval_on_nodes_nod2mod(conf_poisson_tensor_proj, conf_poisson_tensor_at_nodes, gkyl_array_fetch(conf_poisson_tensor, lidx));
   }
+
 
   gkyl_eval_on_nodes_release(h_ij_proj);
   gkyl_eval_on_nodes_release(h_ij_inv_proj);
@@ -128,11 +138,11 @@ gkyl_vlasov_triad_geom_new(const struct gkyl_rect_grid *cgrid, const struct gkyl
   gkyl_eval_on_nodes_release(conf_poisson_tensor_proj);
 
   // free temporary memory
-  gkyl_array_release(cov_tangent_basis_nodal);
-  gkyl_array_release(triad_basis_nodal);
-  gkyl_array_release(triad_basis_gradient_nodal);
-  gkyl_array_release(h_ij_nodal);
-  gkyl_array_release(h_ij_inv_nodal);
-  gkyl_array_release(det_h_nodal);
-  gkyl_array_release(conf_poisson_tensor_nodal);
+  gkyl_array_release(cov_tangent_basis_at_nodes);
+  gkyl_array_release(triad_basis_at_nodes);
+  gkyl_array_release(triad_basis_gradient_at_nodes);
+  gkyl_array_release(h_ij_at_nodes);
+  gkyl_array_release(h_ij_inv_at_nodes);
+  gkyl_array_release(det_h_at_nodes);
+  gkyl_array_release(conf_poisson_tensor_at_nodes);
 }
