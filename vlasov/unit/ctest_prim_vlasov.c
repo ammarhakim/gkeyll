55b45da3d42d1247b874727f68c2534a1b5899c5
johnson452 <grj@princeton.edu>
2025-09-12 14:45:22 -0400

    Fixed unit tests to be consistent with new moment creation. Removed hybrid moments and split the polynomial order 1 test into two tests for order one and order two.

--- vlasov/unit/ctest_prim_vlasov.c
+++ vlasov/unit/ctest_prim_vlasov.c
@@ -2,18 +2,20 @@
 //
 #include <acutest.h>
 #include <math.h>
-#include <gkyl_proj_on_basis.h>
-#include <gkyl_range.h>
-#include <gkyl_rect_decomp.h>
-#include <gkyl_rect_grid.h>
+
 #include <gkyl_array_rio.h>
 #include <gkyl_array_ops.h>
+#include <gkyl_dg_vlasov_calc_hamil.h>
 #include <gkyl_mom_calc_bcorr.h>
 #include <gkyl_mom_calc.h>
 #include <gkyl_mom_vlasov.h>
 #include <gkyl_prim_lbo_calc.h>
 #include <gkyl_prim_lbo_cross_calc.h>
 #include <gkyl_prim_lbo_type.h>
+#include <gkyl_proj_on_basis.h>
+#include <gkyl_range.h>
+#include <gkyl_rect_decomp.h>
+#include <gkyl_rect_grid.h>
 
 static inline double
 maxwellian1D(double n, double vx, double ux, double vth)
@@ -105,7 +107,8 @@ test_func(int cdim, int vdim, int poly_order,
 {
   int pdim = cdim + vdim;  
   double lower[GKYL_MAX_DIM], upper[GKYL_MAX_DIM], confLower[GKYL_MAX_DIM], confUpper[GKYL_MAX_DIM];
-  int cells[GKYL_MAX_DIM], confCells[GKYL_MAX_DIM];
+  double velLower[GKYL_MAX_DIM], velUpper[GKYL_MAX_DIM];
+  int cells[GKYL_MAX_DIM], confCells[GKYL_MAX_DIM], velCells[GKYL_MAX_DIM];
   
   double v_bounds[2*vdim];
   
@@ -123,6 +126,9 @@ test_func(int cdim, int vdim, int poly_order,
       upper[i] = 2.0;
       v_bounds[i - cdim] = lower[i];
       v_bounds[i - cdim + vdim] = upper[i];
+      velLower[i - cdim] = lower[i];
+      velUpper[i - cdim] = upper[i];
+      velCells[i - cdim] = cells[i];
     }
   }
 
@@ -133,8 +139,9 @@ test_func(int cdim, int vdim, int poly_order,
   gkyl_rect_grid_init(&confGrid, cdim, confLower, confUpper, confCells);
 
   // basis functions
-  struct gkyl_basis basis, confBasis;
+  struct gkyl_basis basis, confBasis, velBasis;
   gkyl_cart_modal_serendip(&basis, pdim, poly_order);
+  gkyl_cart_modal_serendip(&velBasis, vdim, poly_order);
   gkyl_cart_modal_serendip(&confBasis, cdim, poly_order);
 
   int confGhost[] = { 0 };
@@ -184,7 +191,33 @@ test_func(int cdim, int vdim, int poly_order,
   // project collision frequency on basis
   gkyl_proj_on_basis_advance(projNu, 0.0, &confLocal_ext, nu);
   
-  struct gkyl_mom_type *vm_moms_t = gkyl_mom_vlasov_new(&confBasis, &basis, GKYL_F_MOMENT_M0M1M2, false);
+  struct gkyl_rect_grid vel_grid;
+  gkyl_rect_grid_init(&vel_grid, vdim, velLower, velUpper, velCells);
+
+  int velGhost[] = {0};
+  struct gkyl_range velLocal, velLocal_ext;
+  gkyl_create_grid_ranges(&vel_grid, velGhost, &velLocal_ext, &velLocal);
+
+  // build hamil and gamma_inv
+  struct gkyl_array *hamil = mkarr(velBasis.num_basis, velLocal.volume);
+  struct gkyl_array *gamma_inv = mkarr(velBasis.num_basis, velLocal.volume);
+  gkyl_dg_vlasov_calc_hamil(&vel_grid, &velBasis, &velLocal, 
+    GKYL_MODEL_DEFAULT, 0, hamil, gamma_inv, false); 
+
+  struct gkyl_mom_vlasov_inp inp_mom = {
+    .conf_basis = &confBasis,
+    .phase_basis = &basis,
+    .vel_range = &velLocal,
+    .use_vmap = 0, 
+    .vmap = 0, 
+    .hamil_range = &velLocal,
+    .hamil = hamil,
+    .model_id = GKYL_MODEL_DEFAULT,
+    .mom_type = GKYL_F_MOMENT_M0M1M2, 
+    .use_gpu = false,
+  };
+
+  struct gkyl_mom_type *vm_moms_t = gkyl_mom_vlasov_inew(&inp_mom);
   gkyl_mom_calc *moms_calc = gkyl_mom_calc_new(&grid, vm_moms_t, false);
 
   // create moment arrays
@@ -246,6 +279,8 @@ test_func(int cdim, int vdim, int poly_order,
     double *uptr = gkyl_array_fetch(u, linc);
     for (unsigned int k=0; k<confBasis.num_basis; ++k) {
       TEST_CHECK( gkyl_compare( u_check[k], uptr[k], 1e-12) );
+      TEST_MSG("Expected: %.13e in cell (%d)", u_check[k], i);
+      TEST_MSG("Produced: %.13e", uptr[k]);
   }}
 
   // Check vtSq.
@@ -256,6 +291,8 @@ test_func(int cdim, int vdim, int poly_order,
     double *vthptr = gkyl_array_fetch(vth, linc);
     for (unsigned int k=0; k<confBasis.num_basis; ++k) {
       TEST_CHECK( gkyl_compare( vth_check[k], vthptr[k], 1e-12) );
+      TEST_MSG("Expected: %.13e in cell (%d)", vth_check[k], i);
+      TEST_MSG("Produced: %.13e", vthptr[k]);
   }}
 
   struct gkyl_array *u_out = mkarr(vdim*confBasis.num_basis, confLocal_ext.volume);
@@ -284,6 +321,8 @@ test_func(int cdim, int vdim, int poly_order,
     double *uptr = gkyl_array_fetch(u_out, linc);
     for (unsigned int k=0; k<confBasis.num_basis; ++k) {
       TEST_CHECK( gkyl_compare( ucross_check[k], uptr[k], 1e-12) );
+      TEST_MSG("Expected: %.13e in cell (%d)", ucross_check[k], i);
+      TEST_MSG("Produced: %.13e", uptr[k]);
   }}
 
   // Check cross vtsq
