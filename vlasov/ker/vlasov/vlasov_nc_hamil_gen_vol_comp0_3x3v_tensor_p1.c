fcef578f670b7988829f5bd564201276580a3490
johnson452 <grj@princeton.edu>
2025-09-28 11:33:09 -0400

    Added 3x3v tensor hybrid kernels for the volume term

---
+++ vlasov/ker/vlasov/vlasov_nc_hamil_gen_vol_comp0_3x3v_tensor_p1.c
@@ -0,0 +1,30 @@
+#include <gkyl_nc_hamil_vol_comps_kernels.h> 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp0_3x3v_tensor_p1(const double *w, const double *dxv, const int dir,
+   const double *alpha, double* GKYL_RESTRICT out) 
+{ 
+  const double dx10 = 2.0/dxv[0]; 
+  const double dx11 = 2.0/dxv[1]; 
+  const double dx12 = 2.0/dxv[2]; 
+  const double dv10 = 2.0/dxv[3]; 
+  const double dv11 = 2.0/dxv[4]; 
+  const double dv12 = 2.0/dxv[5]; 
+  
+  if (dir == 0) { 
+  } 
+
+  if (dir == 1) { 
+  } 
+
+  if (dir == 2) { 
+  } 
+
+  if (dir == 3) { 
+  } 
+
+  if (dir == 4) { 
+  } 
+
+  if (dir == 5) { 
+  } 
+
+} 
