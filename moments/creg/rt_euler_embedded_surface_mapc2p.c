c61eeeb6083b07b3e28d0233c26ebc94c0b3fbe1
kltrbrds <kb8689@princeton.edu>
2025-10-01 11:44:31 -0400

    Code now builds with embedded boundaries for fluids. Except unit tests for all the fluid equation objects, where a bunch of function signatures need to be updated.

--- moments/creg/rt_euler_embedded_surface_mapc2p.c
+++ moments/creg/rt_euler_embedded_surface_mapc2p.c
@@ -266,7 +266,7 @@ main(int argc, char **argv)
     evalPhiInit, NULL, &ctx);
 
   // Fluid equations.
-  struct gkyl_wv_eqn *euler = gkyl_wv_euler_new(ctx.gas_gamma, embed_geo, app_args.use_gpu);
+  struct gkyl_wv_eqn *euler = gkyl_wv_euler_new(ctx.gas_gamma, app_args.use_gpu);
 
   struct gkyl_moment_species fluid = {
     .name = "euler",
