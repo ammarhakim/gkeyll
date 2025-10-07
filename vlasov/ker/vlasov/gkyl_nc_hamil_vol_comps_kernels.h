b5b7e39b8a88375db904421c939c2349a7399c22
johnson452 <grj@princeton.edu>
2025-10-07 15:26:08 -0400

    Added in 2x3v p2 Ser kernels for triad

---
+++ vlasov/ker/vlasov/gkyl_nc_hamil_vol_comps_kernels.h
@@ -0,0 +1,233 @@
+#pragma once 
+#include <math.h> 
+#include <gkyl_util.h> 
+EXTERN_C_BEG 
+
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_alpha_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *poisson_tensor_conf, const double *hamil, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp0_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp1_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp2_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp3_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp4_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp5_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp6_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp7_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp8_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp9_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp10_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp11_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp12_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp13_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp14_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp15_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp16_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp17_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp18_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp19_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp20_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp21_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp22_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp23_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp24_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp25_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp26_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp27_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp28_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp29_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp30_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp31_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp32_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp33_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp34_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp35_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp36_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp37_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp38_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp39_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp40_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp41_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp42_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp43_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp44_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp45_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp46_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp47_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp48_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp49_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp50_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp51_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp52_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp53_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp54_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp55_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp56_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp57_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp58_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp59_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp60_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp61_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp62_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp63_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp64_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp65_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp66_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp67_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp68_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp69_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp70_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp71_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp72_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp73_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp74_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp75_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp76_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp77_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp78_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp79_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp80_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp81_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp82_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp83_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp84_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp85_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp86_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp87_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp88_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp89_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp90_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp91_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp92_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp93_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp94_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp95_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp96_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp97_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp98_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp99_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp100_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp101_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp102_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp103_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp104_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp105_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp106_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp107_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp108_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp109_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp110_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+GKYL_CU_DH void vlasov_nc_hamil_gen_vol_comp111_2x3v_ser_p2(const double *w, const double *dxv, const int dir,
+     const double *alpha, double* GKYL_RESTRICT out); 
+
+EXTERN_C_END 
