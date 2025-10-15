ab6a56680862a293fa636c7fe1d8e2ece6062737
akashukla <akashukla@utexas.edu>
2025-10-15 11:03:28 -0500

    rename tok calc derived geo to rz calc derived geo because it is actually for both tokamaks and mirrors which make use of rz coordinates gkylcas d1e72934361cd9f0c9be88bac3b4800a7c87f51a

---
+++ gyrokinetic/ker/derived_geo/gkyl_rz_calc_derived_geo_kernels.h
@@ -0,0 +1,10 @@
+#pragma once 
+#include <math.h> 
+#include <stdbool.h> 
+#include <gkyl_util.h> 
+EXTERN_C_BEG 
+
+GKYL_CU_DH void rz_derived_geo_3x_Ser_p1(const double *gij, const double *bmag, const double *J, double *Jinv, double *grij, double *bi, double *cmag, double *Jtot, double *Jtotinv, double *gxxJ, double *gxyJ, double *gyyJ, double *gxzJ, double *eps2); 
+GKYL_CU_DH void rz_derived_geo_quad_3x_Ser_p1(const double *gij, const double *bmag, const double *J, double *Jinv, double *grij, double *bi, double *cmag, double *Jtot, double *Jtotinv, double *gxxJ, double *gxyJ, double *gyyJ, double *gxzJ, double *eps2); 
+GKYL_CU_DH void rz_derived_geo_3x_Ser_p2(const double *gij, const double *bmag, const double *J, double *Jinv, double *grij, double *bi, double *cmag, double *Jtot, double *Jtotinv, double *gxxJ, double *gxyJ, double *gyyJ, double *gxzJ, double *eps2); 
+EXTERN_C_END 
