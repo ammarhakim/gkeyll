67a298f8fb8cf4c9db3cfcb8bd603b8591017948
johnson452 <grj@princeton.edu>
2025-10-17 16:20:22 -0400

    Restore 2x3v kernels for p = 2 triad.

--- vlasov/zero/gkyl_dg_vlasov_priv.h
+++ vlasov/zero/gkyl_dg_vlasov_priv.h
@@ -719,7 +719,7 @@ static const gkyl_dg_vlasov_stream_surf_from_flux_kern_list ser_stream_nc_hamil_
   { NULL, NULL, NULL, NULL }, // 0
   { NULL, NULL, NULL, NULL }, // 1
   { NULL, NULL, NULL, NULL }, // 2  
-  // 2x kernels
+// 2x kernels
   { NULL, NULL, NULL, NULL }, // 3  
   { NULL, NULL, NULL, NULL }, // 4
   { NULL, NULL, NULL, NULL }, // 5
@@ -828,7 +828,7 @@ static const gkyl_dg_vlasov_accel_surf_kern_list ser_accel_ho_surf_vx_kernels[] 
   // 2x kernels
   { NULL, vlasov_surfvx_2x1v_ser_p1, vlasov_ho_surfvx_2x1v_ser_p2, vlasov_ho_surfvx_2x1v_ser_p3 }, // 3
   { NULL, vlasov_surfvx_2x2v_ser_p1, vlasov_ho_surfvx_2x2v_ser_p2, NULL }, // 4
-  { NULL, vlasov_surfvx_2x3v_ser_p1, NULL, NULL }, // 5
+  { NULL, vlasov_surfvx_2x3v_ser_p1, vlasov_ho_surfvx_2x3v_ser_p2, NULL }, // 5
   // 3x kernels
   { NULL, vlasov_surfvx_3x3v_ser_p1, NULL, NULL }, // 6
 };
@@ -843,7 +843,7 @@ static const gkyl_dg_vlasov_accel_surf_kern_list ser_accel_ho_surf_vy_kernels[] 
   // 2x kernels
   { NULL, NULL, NULL, NULL }, // 3
   { NULL, vlasov_surfvy_2x2v_ser_p1, vlasov_ho_surfvy_2x2v_ser_p2, NULL }, // 4
-  { NULL, vlasov_surfvy_2x3v_ser_p1, NULL, NULL }, // 5
+  { NULL, vlasov_surfvy_2x3v_ser_p1, vlasov_ho_surfvy_2x3v_ser_p2, NULL }, // 5
   // 3x kernels
   { NULL, vlasov_surfvy_3x3v_ser_p1, NULL, NULL }, // 6
 };
@@ -858,7 +858,7 @@ static const gkyl_dg_vlasov_accel_surf_kern_list ser_accel_ho_surf_vz_kernels[] 
   // 2x kernels
   { NULL, NULL, NULL, NULL }, // 3
   { NULL, NULL, NULL, NULL }, // 4
-  { NULL, vlasov_surfvz_2x3v_ser_p1, NULL, NULL }, // 5
+  { NULL, vlasov_surfvz_2x3v_ser_p1, vlasov_ho_surfvz_2x3v_ser_p2, NULL }, // 5
   // 3x kernels
   { NULL, vlasov_surfvz_3x3v_ser_p1, NULL, NULL }, // 6
 };
@@ -1234,7 +1234,7 @@ static const gkyl_dg_vlasov_accel_boundary_surf_kern_list ser_accel_boundary_ho_
   // 2x kernels
   { NULL, vlasov_boundary_surfvx_2x1v_ser_p1, vlasov_boundary_ho_surfvx_2x1v_ser_p2, vlasov_boundary_ho_surfvx_2x1v_ser_p3 }, // 3
   { NULL, vlasov_boundary_surfvx_2x2v_ser_p1, vlasov_boundary_ho_surfvx_2x2v_ser_p2, NULL }, // 4
-  { NULL, vlasov_boundary_surfvx_2x3v_ser_p1, NULL, NULL }, // 5
+  { NULL, vlasov_boundary_surfvx_2x3v_ser_p1, vlasov_boundary_ho_surfvx_2x3v_ser_p2, NULL }, // 5
   // 3x kernels
   { NULL, vlasov_boundary_surfvx_3x3v_ser_p1, NULL, NULL }, // 6
 };
