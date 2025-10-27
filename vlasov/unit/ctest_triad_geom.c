443a37b745a39def7661fe3526d34a6c474a0047
johnson452 <grj@princeton.edu>
2025-10-26 22:29:40 -0400

    One more false flag to unit test when directly creating triad_geom_inp

--- vlasov/unit/ctest_triad_geom.c
+++ vlasov/unit/ctest_triad_geom.c
@@ -147,6 +147,7 @@ test_triad_1x1v_flat_conf(int poly_order)
 
   // Construct the input map
   struct gkyl_vlasov_triad_geom_inp inp_basis_vectors;
+  inp_basis_vectors.use_vierbein = false;
   inp_basis_vectors.eval_cov_tangent_basis = eval_cov_tangent_basis_1v; 
   inp_basis_vectors.eval_triad_basis = eval_triad_basis_1v; 
   inp_basis_vectors.eval_triad_basis_gradient = eval_triad_basis_gradient_1v; 
