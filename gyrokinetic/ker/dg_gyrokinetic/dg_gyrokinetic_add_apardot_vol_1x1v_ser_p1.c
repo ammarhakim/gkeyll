314530e87bd3fbf56269dedd76da63132c756023
antoinehoff <antoine-hoffmann@hotmail.com>
2025-11-07 11:24:29 -0500

    fix an error were apardot was expanded on bP and not bC

--- gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apardot_vol_1x1v_ser_p1.c
+++ gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apardot_vol_1x1v_ser_p1.c
@@ -8,18 +8,14 @@ GKYL_CU_DH double dg_gyrokinetic_add_apardot_vol_1x1v_ser_p1(const double q_, co
   // out: output increment.
 
   double alphavpar[6] = {0.}; 
-  alphavpar[0] = -((1.0*apardot[0]*q_)/m_); 
-  alphavpar[1] = -((1.0*apardot[1]*q_)/m_); 
-  alphavpar[2] = -((1.0*apardot[2]*q_)/m_); 
-  alphavpar[3] = -((1.0*apardot[3]*q_)/m_); 
-  alphavpar[4] = -((1.0*apardot[4]*q_)/m_); 
-  alphavpar[5] = -((1.0*apardot[5]*q_)/m_); 
+  alphavpar[0] = -((1.4142135623730951*apardot[0]*q_)/m_); 
+  alphavpar[1] = -((1.4142135623730951*apardot[1]*q_)/m_); 
 
 
-  out[2] += 0.8660254037844386*(alphavpar[5]*fin[5]+alphavpar[4]*fin[4]+alphavpar[3]*fin[3]+alphavpar[2]*fin[2]+alphavpar[1]*fin[1]+alphavpar[0]*fin[0]); 
-  out[3] += 0.8660254037844387*(alphavpar[4]*fin[5]+fin[4]*alphavpar[5])+0.8660254037844386*(alphavpar[2]*fin[3]+fin[2]*alphavpar[3]+alphavpar[0]*fin[1]+fin[0]*alphavpar[1]); 
-  out[4] += 1.7320508075688774*(alphavpar[3]*fin[5]+fin[3]*alphavpar[5])+1.7320508075688772*(alphavpar[2]*fin[4]+fin[2]*alphavpar[4])+1.9364916731037085*(alphavpar[1]*fin[3]+fin[1]*alphavpar[3]+alphavpar[0]*fin[2]+fin[0]*alphavpar[2]); 
-  out[5] += 1.7320508075688772*(alphavpar[2]*fin[5]+fin[2]*alphavpar[5])+1.7320508075688774*(alphavpar[3]*fin[4]+fin[3]*alphavpar[4])+1.9364916731037085*(alphavpar[0]*fin[3]+fin[0]*alphavpar[3]+alphavpar[1]*fin[2]+fin[1]*alphavpar[2]); 
+  out[2] += 0.8660254037844386*(alphavpar[1]*fin[1]+alphavpar[0]*fin[0]); 
+  out[3] += 0.8660254037844386*(alphavpar[0]*fin[1]+fin[0]*alphavpar[1]); 
+  out[4] += 1.9364916731037085*(alphavpar[1]*fin[3]+alphavpar[0]*fin[2]); 
+  out[5] += 1.9364916731037085*(alphavpar[0]*fin[3]+alphavpar[1]*fin[2]); 
 
   return 0.; 
 } 
