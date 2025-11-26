58afe71ab18c159aa81d684ef17984c00a9cd869
Jaime Manuel Caballero Tello <jcaballe@login04.pitagora.local>
2025-11-26 17:40:26 +0100

    update configuration files pitagora-gpu, back to nccl 2.18

--- machines/mkdeps.pitagora-gpu.sh
+++ machines/mkdeps.pitagora-gpu.sh
@@ -1,8 +1,9 @@
 # Loading modules, pitagora currently only supports cuda 12.6 
 
+module load spack/0.22.2_6.1
 module load cuda/12.6
-module load module load openmpi/4.1.6--gcc--12.3.0
-module load nccl/2.22.3-1--gcc--12.3.0
+module load openmpi/4.1.6--gcc--12.3.0
+spack load nccl@2.18.3-1 %gcc@12.3.0
 module load cmake/3.27.9
 
 cd install-deps
