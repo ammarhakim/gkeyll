331ec9f6287a8e0f728a3b7761c1f82363b35e9f
antoinehoff <antoine-hoffmann@hotmail.com>
2025-12-08 16:31:45 -0500

    reduction of the rt alfven 1x2v frame number to 1

--- gyrokinetic/creg/rt_gk_em_alfven_1x2v_p1.c
+++ gyrokinetic/creg/rt_gk_em_alfven_1x2v_p1.c
@@ -232,9 +232,9 @@ create_ctx(void)
   int poly_order = 1;
 
   double t_end = nperiod*2*M_PI/omega; // End time [s].
-  int num_frames = 100;
+  int num_frames = 1;
   double write_phase_freq = 1.0; // Frequency of writing phase-space diagnostics (as a fraction of num_frames).
-  int int_diag_calc_num = num_frames*1000;
+  int int_diag_calc_num = num_frames*100;
   double dt_failure_tol = 1.0e-4; // Minimum allowable fraction of initial time-step.
   int num_failures_max = 50; // Maximum allowable number of consecutive small time-steps.
 
