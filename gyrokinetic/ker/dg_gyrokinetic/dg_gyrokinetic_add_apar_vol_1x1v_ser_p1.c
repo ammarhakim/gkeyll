33731ed2396a5ca7a29b02f91966b86b39afb10d
antoinehoff <antoine-hoffmann@hotmail.com>
2026-02-18 09:21:30 -0500

    These new apar volume kernels use the product rule to compute ``` curl(Apar b)/mB ``` as ``` Apar/m curl(b)/B + grad(Apar)/m x b/B ``` so that we make full use of predefined geometric quantities used in the electrostatic volume kernel, i.e. - `dualcurlbhatoverB` for curl(b)/B - `bioverJB` for the cross product with b/B

--- gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apar_vol_1x1v_ser_p1.c
+++ gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apar_vol_1x1v_ser_p1.c
@@ -1,6 +1,6 @@
 #include <gkyl_dg_gyrokinetic_kernels.h> 
 GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x1v_ser_p1(const double *w, const double *dxv, const double *vmap, const double *vmapSq,
-    const double q_, const double m_, const double *bmag, const double *jacobtot_inv,
+    const double q_, const double m_, const double *bmag, const double *jacobtot_inv, const double *dualcurlbhatoverB, const double *bioverJB,
     const double *b_i, const double *phi, const double *apar, const double *fin, double* GKYL_RESTRICT out) 
 { 
   // w[NDIM]: cell-center.
@@ -10,6 +10,8 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x1v_ser_p1(const double *w, const
   // q_,m_: species charge and mass.
   // bmag: magnetic field amplitude.
   // jacobtot_inv: reciprocal of the conf-space jacobian time the guiding center coordinate Jacobian.
+  // dualcurlbhatoverB: dual curl of bhat over B.
+  // bioverJB: b_i over J times B.
   // b_i: covariant components of the field aligned unit vector.
   // apar: parallel component of magnetic vector potential.
   // phi: electrostatic potential .
@@ -26,6 +28,14 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x1v_ser_p1(const double *w, const
   const double *b_y = &b_i[2];
   const double *b_z = &b_i[4];
 
+  const double *bioverJB_x = &bioverJB[0]; 
+  const double *bioverJB_y = &bioverJB[2]; 
+  const double *bioverJB_z = &bioverJB[4]; 
+
+  const double *dualcurlbhatoverB_x = &dualcurlbhatoverB[0]; 
+  const double *dualcurlbhatoverB_y = &dualcurlbhatoverB[2]; 
+  const double *dualcurlbhatoverB_z = &dualcurlbhatoverB[4]; 
+
   double hamil[6] = {0.}; 
   hamil[0] = 1.4142135623730951*phi[0]*q_+0.7071067811865475*vmapSq[0]*m_; 
   hamil[1] = 1.4142135623730951*phi[1]*q_; 
@@ -39,12 +49,25 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x1v_ser_p1(const double *w, const
   hamil2[1] = hamil[4]*hamil[4]; 
 
   double alphax[6] = {0.}; 
+  alphax[0] = ((0.7071067811865475*apar[1]*dualcurlbhatoverB_z[1]*hamil[2]+0.7071067811865475*apar[0]*dualcurlbhatoverB_z[0]*hamil[2])*rdx2)/(vmap[1]*m_); 
+  alphax[1] = ((0.7071067811865475*apar[0]*dualcurlbhatoverB_z[1]*hamil[2]+0.7071067811865475*dualcurlbhatoverB_z[0]*apar[1]*hamil[2])*rdx2)/(vmap[1]*m_); 
+  alphax[2] = ((1.5811388300841895*apar[1]*dualcurlbhatoverB_z[1]*hamil[4]+1.5811388300841895*apar[0]*dualcurlbhatoverB_z[0]*hamil[4])*rdx2)/(vmap[1]*m_); 
+  alphax[3] = ((1.5811388300841895*apar[0]*dualcurlbhatoverB_z[1]*hamil[4]+1.5811388300841895*dualcurlbhatoverB_z[0]*apar[1]*hamil[4])*rdx2)/(vmap[1]*m_); 
 
 
+  out[1] += 0.8660254037844386*(alphax[3]*fin[3]+alphax[2]*fin[2]+alphax[1]*fin[1]+alphax[0]*fin[0]); 
+  out[3] += 0.7745966692414834*alphax[3]*fin[5]+0.7745966692414833*alphax[2]*fin[4]+0.8660254037844386*(alphax[1]*fin[3]+fin[1]*alphax[3]+alphax[0]*fin[2]+fin[0]*alphax[2]); 
+  out[5] += 0.8660254037844386*alphax[1]*fin[5]+0.8660254037844387*alphax[0]*fin[4]+0.7745966692414834*(alphax[3]*fin[3]+alphax[2]*fin[2]); 
 
   double alphavpar[6] = {0.}; 
+  alphavpar[0] = ((-(0.7071067811865475*apar[1]*dualcurlbhatoverB_z[1]*hamil[1])-0.7071067811865475*apar[0]*dualcurlbhatoverB_z[0]*hamil[1])*rdx2)/(vmap[1]*m_); 
+  alphavpar[1] = ((-(0.7071067811865475*apar[0]*dualcurlbhatoverB_z[1]*hamil[1])-0.7071067811865475*dualcurlbhatoverB_z[0]*apar[1]*hamil[1])*rdx2)/(vmap[1]*m_); 
 
 
+  out[2] += 0.8660254037844386*(alphavpar[1]*fin[1]+alphavpar[0]*fin[0]); 
+  out[3] += 0.8660254037844386*(alphavpar[0]*fin[1]+fin[0]*alphavpar[1]); 
+  out[4] += 1.9364916731037085*(alphavpar[1]*fin[3]+alphavpar[0]*fin[2]); 
+  out[5] += 1.9364916731037085*(alphavpar[0]*fin[3]+alphavpar[1]*fin[2]); 
 
   return 0.; 
 } 
--- gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apar_vol_1x2v_ser_p1.c
+++ gyrokinetic/ker/dg_gyrokinetic/dg_gyrokinetic_add_apar_vol_1x2v_ser_p1.c
@@ -1,6 +1,6 @@
 #include <gkyl_dg_gyrokinetic_kernels.h> 
 GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x2v_ser_p1(const double *w, const double *dxv, const double *vmap, const double *vmapSq,
-    const double q_, const double m_, const double *bmag, const double *jacobtot_inv,
+    const double q_, const double m_, const double *bmag, const double *jacobtot_inv, const double *dualcurlbhatoverB, const double *bioverJB,
     const double *b_i, const double *phi, const double *apar, const double *fin, double* GKYL_RESTRICT out) 
 { 
   // w[NDIM]: cell-center.
@@ -10,6 +10,8 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x2v_ser_p1(const double *w, const
   // q_,m_: species charge and mass.
   // bmag: magnetic field amplitude.
   // jacobtot_inv: reciprocal of the conf-space jacobian time the guiding center coordinate Jacobian.
+  // dualcurlbhatoverB: dual curl of bhat over B.
+  // bioverJB: b_i over J times B.
   // b_i: covariant components of the field aligned unit vector.
   // apar: parallel component of magnetic vector potential.
   // phi: electrostatic potential .
@@ -27,6 +29,14 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x2v_ser_p1(const double *w, const
   const double *b_y = &b_i[2];
   const double *b_z = &b_i[4];
 
+  const double *bioverJB_x = &bioverJB[0]; 
+  const double *bioverJB_y = &bioverJB[2]; 
+  const double *bioverJB_z = &bioverJB[4]; 
+
+  const double *dualcurlbhatoverB_x = &dualcurlbhatoverB[0]; 
+  const double *dualcurlbhatoverB_y = &dualcurlbhatoverB[2]; 
+  const double *dualcurlbhatoverB_z = &dualcurlbhatoverB[4]; 
+
   double hamil[12] = {0.}; 
   hamil[0] = 2.0*phi[0]*q_+vmapSq[0]*m_+1.4142135623730951*bmag[0]*vmap[2]; 
   hamil[1] = 2.0*phi[1]*q_+1.4142135623730951*bmag[1]*vmap[2]; 
@@ -42,12 +52,34 @@ GKYL_CU_DH double dg_gyrokinetic_add_apar_vol_1x2v_ser_p1(const double *w, const
   hamil2[1] = hamil[8]*hamil[8]; 
 
   double alphax[12] = {0.}; 
+  alphax[0] = ((0.7071067811865475*apar[1]*dualcurlbhatoverB_z[1]*hamil[2]+0.7071067811865475*apar[0]*dualcurlbhatoverB_z[0]*hamil[2])*rdx2)/(vmap[1]*m_); 
+  alphax[1] = ((0.7071067811865475*apar[0]*dualcurlbhatoverB_z[1]*hamil[2]+0.7071067811865475*dualcurlbhatoverB_z[0]*apar[1]*hamil[2])*rdx2)/(vmap[1]*m_); 
+  alphax[2] = ((1.5811388300841895*apar[1]*dualcurlbhatoverB_z[1]*hamil[8]+1.5811388300841895*apar[0]*dualcurlbhatoverB_z[0]*hamil[8])*rdx2)/(vmap[1]*m_); 
+  alphax[4] = ((1.5811388300841895*apar[0]*dualcurlbhatoverB_z[1]*hamil[8]+1.5811388300841895*dualcurlbhatoverB_z[0]*apar[1]*hamil[8])*rdx2)/(vmap[1]*m_); 
 
 
+  out[1] += 0.6123724356957944*(alphax[4]*fin[4]+alphax[2]*fin[2]+alphax[1]*fin[1]+alphax[0]*fin[0]); 
+  out[4] += 0.5477225575051661*(alphax[4]*fin[9]+alphax[2]*fin[8])+0.6123724356957944*(alphax[1]*fin[4]+fin[1]*alphax[4]+alphax[0]*fin[2]+fin[0]*alphax[2]); 
+  out[5] += 0.6123724356957944*(alphax[4]*fin[7]+alphax[2]*fin[6]+alphax[1]*fin[5]+alphax[0]*fin[3]); 
+  out[7] += 0.5477225575051661*(alphax[4]*fin[11]+alphax[2]*fin[10])+0.6123724356957944*(alphax[1]*fin[7]+alphax[0]*fin[6]+alphax[4]*fin[5]+alphax[2]*fin[3]); 
+  out[9] += 0.6123724356957944*(alphax[1]*fin[9]+alphax[0]*fin[8])+0.5477225575051661*(alphax[4]*fin[4]+alphax[2]*fin[2]); 
+  out[11] += 0.6123724356957944*(alphax[1]*fin[11]+alphax[0]*fin[10])+0.5477225575051661*(alphax[4]*fin[7]+alphax[2]*fin[6]); 
 
   double alphavpar[12] = {0.}; 
+  alphavpar[0] = ((-(0.7071067811865475*apar[1]*dualcurlbhatoverB_z[1]*hamil[1])-0.7071067811865475*apar[0]*dualcurlbhatoverB_z[0]*hamil[1])*rdx2)/(vmap[1]*m_); 
+  alphavpar[1] = ((-(0.7071067811865475*apar[0]*dualcurlbhatoverB_z[1]*hamil[1])-0.7071067811865475*dualcurlbhatoverB_z[0]*apar[1]*hamil[1])*rdx2)/(vmap[1]*m_); 
+  alphavpar[3] = ((-(0.7071067811865475*apar[1]*dualcurlbhatoverB_z[1]*hamil[5])-0.7071067811865475*apar[0]*dualcurlbhatoverB_z[0]*hamil[5])*rdx2)/(vmap[1]*m_); 
+  alphavpar[5] = ((-(0.7071067811865475*apar[0]*dualcurlbhatoverB_z[1]*hamil[5])-0.7071067811865475*dualcurlbhatoverB_z[0]*apar[1]*hamil[5])*rdx2)/(vmap[1]*m_); 
 
 
+  out[2] += 0.6123724356957944*(alphavpar[5]*fin[5]+alphavpar[3]*fin[3]+alphavpar[1]*fin[1]+alphavpar[0]*fin[0]); 
+  out[4] += 0.6123724356957944*(alphavpar[3]*fin[5]+fin[3]*alphavpar[5]+alphavpar[0]*fin[1]+fin[0]*alphavpar[1]); 
+  out[6] += 0.6123724356957944*(alphavpar[1]*fin[5]+fin[1]*alphavpar[5]+alphavpar[0]*fin[3]+fin[0]*alphavpar[3]); 
+  out[7] += 0.6123724356957944*(alphavpar[0]*fin[5]+fin[0]*alphavpar[5]+alphavpar[1]*fin[3]+fin[1]*alphavpar[3]); 
+  out[8] += 1.369306393762915*(alphavpar[5]*fin[7]+alphavpar[3]*fin[6]+alphavpar[1]*fin[4]+alphavpar[0]*fin[2]); 
+  out[9] += 1.369306393762915*(alphavpar[3]*fin[7]+alphavpar[5]*fin[6]+alphavpar[0]*fin[4]+alphavpar[1]*fin[2]); 
+  out[10] += 1.369306393762915*(alphavpar[1]*fin[7]+alphavpar[0]*fin[6]+fin[4]*alphavpar[5]+fin[2]*alphavpar[3]); 
+  out[11] += 1.369306393762915*(alphavpar[0]*fin[7]+alphavpar[1]*fin[6]+fin[2]*alphavpar[5]+alphavpar[3]*fin[4]); 
 
   return 0.; 
 } 
