f3b6452fc39c58bbbe6abaa0ed8ae885e504897d
manauref <mfrancisquez@gmail.com>
2026-02-23 12:57:47 -0700

    Cosmetic changes to unit test for readability/adhering to better practices. Also fixed a missing factor of 2.

--- machines/mkdeps.macos.sh
+++ machines/mkdeps.macos.sh
@@ -1,4 +1,4 @@
 cd install-deps
-: "${PREFIX:=$HOME/gkylsoft}"
+: "${PREFIX:=/Users/mfrancis/Documents/gkeyll/code/gkeyll_v3/gkylsoft}"
 export MACOSX_DEPLOYMENT_TARGET=14.7
-./mkdeps.sh --build-openblas=no --build-superlu=yes --build-luajit=yes --prefix=$PREFIX
+./mkdeps.sh --build-openblas=no --build-superlu=yes --build-luajit=yes --build-openmpi=yes --prefix=$PREFIX
