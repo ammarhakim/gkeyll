b1f0e6d167928213721c85c01b0e99f3e45e4409
Manaure Francisquez <mfrancisquez@gmail.com>
2026-02-24 21:06:53 -0800

    Update perlmutter CPU machine files so they use mpi and NERSC's lapack. Tested it and it works.

--- machines/configure.perlmutter.gpu.sh
+++ machines/configure.perlmutter.gpu.sh
@@ -1,8 +1,3 @@
-#. This is the loaded modules when the compilation and test succeded on Perlmutter GPU (ACDH 02/2025)
-#.  1) craype-x86-milan                                5) gcc-native/13.2          9) sqs/2.0               13) cray-libsci/24.07.0             17) nccl/2.18.3-cu12 (g)
-#.  2) libfabric/1.20.1                                6) perftools-base/24.07.0  10) darshan/default       14) PrgEnv-gnu/8.5.0      (prgenv)  18) cudatoolkit/12.4 (g)
-#.  3) craype-network-ofi                              7) cpe/24.07               11) craype/2.7.32    (c)  15) craype-accel-nvidia80 (cpe)
-#.  4) xpmem/2.9.6-1.1_20240510205610__g087dc11fc19d   8) gpu/1.0                 12) cray-dsmml/0.3.0      16) cray-mpich/8.1.28     (mpi)
 module load PrgEnv-gnu/8.6.0
 module load craype-accel-nvidia80
 module load cray-mpich/9.0.1
