0a73249b8b9e91e071ef38aa12bededcbd23e449
Akash Shukla <akashukla@utexas.edu>
2026-04-10 13:47:11 -0700

    Change build-eirene-coupling to use the released version

--- install-deps/build-eirene-coupling.sh
+++ install-deps/build-eirene-coupling.sh
@@ -8,8 +8,10 @@ echo ">>> Starting GK-Neutral_coupling Installation..."
 cd ../../gkylsoft/
 
 if [ ! -d "GK-Neutral_coupling" ]; then
-    echo ">>> Cloning GK-Neutral_coupling..."
-    git clone https://github.com/jRoeltgen/GK-Neutral_coupling.git
+    echo ">>> Downloading GK-Neutral_coupling..."
+    wget https://github.com/jRoeltgen/GK-Neutral_coupling/archive/refs/tags/gkeyll.tar.gz
+    mkdir GK-Neutral_coupling && tar -xzf gkeyll.tar.gz -C GK-Neutral_coupling --strip-components=1
+    rm gkeyll.tar.gz
 else
     echo ">>> GK-Neutral_coupling directory already exists. Skipping clone."
 fi
