a611330d65b3b537487f9961c5f7666d6d44230c
Antoinehoff <antoine-hoffmann@hotmail.com>
2026-04-15 14:07:56 -0700

    use the sheath surrogate in the helical regression test

--- gyrokinetic/creg/rt_gk_helical_3x2v_p1.c
+++ gyrokinetic/creg/rt_gk_helical_3x2v_p1.c
@@ -556,8 +556,10 @@ main(int argc, char **argv)
     .bcs = {
       { .dir = 0, .edge = GKYL_LOWER_EDGE, .type = GKYL_BC_GK_SPECIES_ZERO_FLUX },
       { .dir = 0, .edge = GKYL_UPPER_EDGE, .type = GKYL_BC_GK_SPECIES_ZERO_FLUX },
-      { .dir = 2, .edge = GKYL_LOWER_EDGE, .type = GKYL_BC_GK_SPECIES_SHEATH },
-      { .dir = 2, .edge = GKYL_UPPER_EDGE, .type = GKYL_BC_GK_SPECIES_SHEATH },
+      { .dir = 2, .edge = GKYL_LOWER_EDGE, .type = GKYL_BC_GK_SPECIES_SHEATH,
+        .use_sheath_surrogate = true },
+      { .dir = 2, .edge = GKYL_UPPER_EDGE, .type = GKYL_BC_GK_SPECIES_SHEATH,
+        .use_sheath_surrogate = true },
     },
 
     .num_diag_moments = 9,
