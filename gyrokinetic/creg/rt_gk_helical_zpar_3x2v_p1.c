62460ee469b15e1076e9c2a1736f634a0724fb1b
Antoinehoff <antoine-hoffmann@hotmail.com>
2026-04-29 13:43:58 -0700

    set default rt naming

--- gyrokinetic/creg/rt_gk_helical_zpar_3x2v_p1.c
+++ gyrokinetic/creg/rt_gk_helical_zpar_3x2v_p1.c
@@ -572,7 +572,6 @@ main(int argc, char **argv)
 
   // GK app
   struct gkyl_gk app_inp = {
-    .name = "gk_em_helical_sol_3x2v_p1",
 
     .cfl_frac_omegaH = 1.0,
     .cfl_frac = 0.9,
@@ -606,6 +605,7 @@ main(int argc, char **argv)
     }
   };
 
+  snprintf(app_inp.name, sizeof(app_inp.name), "%s", app_args.app_name);
   struct gkyl_gyrokinetic_run_inp run_inp = {
     .app_inp = app_inp,
     .time_stepping = {
