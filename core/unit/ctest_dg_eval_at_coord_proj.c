95d7e1fd797e420796c324fa4f7095088ad7efea
manauref <mfrancisquez@gmail.com>
2026-06-24 12:42:10 -0700

    Make a couple of changees to make dg_evproj unit test work on GPUs.

--- core/unit/ctest_dg_eval_at_coord_proj.c
+++ core/unit/ctest_dg_eval_at_coord_proj.c
@@ -10,6 +10,14 @@
 #include <gkyl_util.h>
 #include <gkyl_dg_eval_at_coord_proj.h>
 
+static struct gkyl_array*
+mkarr(bool use_gpu, long nc, long size)
+{
+  // Allocate array (filled with zeros).
+  return use_gpu? gkyl_array_cu_dev_new(GKYL_DOUBLE, nc, size)
+    : gkyl_array_new(GKYL_DOUBLE, nc, size);
+}
+
 static void
 eval_f_2d(double t, const double *xn, double *restrict fout, void *ctx)
 {
@@ -26,7 +34,7 @@ eval_f_1d(double t, const double *xn, double *restrict fout, void *ctx)
 }
 
 static void
-test_2x_to_1x(int poly_order)
+test_2x_to_1x(int poly_order, bool use_gpu)
 {
   // Project a 2D DG field onto 1D by evaluating in x at a fixed
   // computational coordinate x0. Verified by comparing against a direct 1D
@@ -62,24 +70,30 @@ test_2x_to_1x(int poly_order)
   gkyl_create_grid_ranges(&grid1, ghost_tar, &local_ext_tar, &local_tar);
 
   // Project f(x,y).
-  struct gkyl_array *fdo = gkyl_array_new(GKYL_DOUBLE, basis_do.num_basis, local_ext_do.volume);
+  struct gkyl_array *fdo = mkarr(use_gpu, basis_do.num_basis, local_ext_do.volume);
+  struct gkyl_array *fdo_ho = use_gpu? mkarr(false, fdo->ncomp, fdo->size)
+                                     : gkyl_array_acquire(fdo_ho);
+  
   gkyl_proj_on_basis *proj_do = gkyl_proj_on_basis_new(&grid2, &basis_do,
     poly_order+1, 1, eval_f_2d, NULL);
-  gkyl_proj_on_basis_advance(proj_do, 0.0, &local_do, fdo);
+  gkyl_proj_on_basis_advance(proj_do, 0.0, &local_do, fdo_ho);
+  gkyl_array_copy(fdo, fdo_ho);
 
   // Project f(x0,y) directly onto the 1D basis.
-  struct gkyl_array *fref = gkyl_array_new(GKYL_DOUBLE, basis_tar.num_basis, local_ext_tar.volume);
+  struct gkyl_array *fref_ho = mkarr(false, basis_tar.num_basis, local_ext_tar.volume);
   gkyl_proj_on_basis *proj_tar = gkyl_proj_on_basis_new(&grid1, &basis_tar,
     poly_order+1, 1, eval_f_1d, &x0);
-  gkyl_proj_on_basis_advance(proj_tar, 0.0, &local_tar, fref);
+  gkyl_proj_on_basis_advance(proj_tar, 0.0, &local_tar, fref_ho);
 
   // Create the updater: evaluate in x (dir 0), keep y (1D target).
   int eval_dirs[] = {0};
   struct gkyl_dg_eval_at_coord_proj *up = gkyl_dg_eval_at_coord_proj_new(
-    &basis_do, &basis_tar, 1, eval_dirs, false);
+    &basis_do, &basis_tar, 1, eval_dirs, use_gpu);
 
   // Apply updater at x = x0.
   struct gkyl_array *ftar = gkyl_array_new(GKYL_DOUBLE, basis_tar.num_basis, local_ext_tar.volume);
+  struct gkyl_array *ftar_ho = use_gpu? mkarr(false, ftar->ncomp, ftar->size)
+                                      : gkyl_array_acquire(ftar);
   double eval_coords[] = {x0};
   bool pick_lower[] = {false, false};
   int known_index[] = {-1, -1};
@@ -87,12 +101,13 @@ test_2x_to_1x(int poly_order)
     &local_do, &local_tar, fdo, ftar);
 
   // Check answer.
+  gkyl_array_copy(ftar_ho, ftar);
   struct gkyl_range_iter iter;
   gkyl_range_iter_init(&iter, &local_tar);
   while (gkyl_range_iter_next(&iter)) {
     long linidx = gkyl_range_idx(&local_tar, iter.idx);
-    const double *ftar_c = gkyl_array_cfetch(ftar, linidx);
-    const double *fref_c = gkyl_array_cfetch(fref, linidx);
+    const double *ftar_c = gkyl_array_cfetch(ftar_ho, linidx);
+    const double *fref_c = gkyl_array_cfetch(fref_ho, linidx);
     for (int k = 0; k < basis_tar.num_basis; k++)
       TEST_CHECK(gkyl_compare(ftar_c[k], fref_c[k], 1e-14));
   }
@@ -101,15 +116,22 @@ test_2x_to_1x(int poly_order)
   gkyl_proj_on_basis_release(proj_do);
   gkyl_proj_on_basis_release(proj_tar);
   gkyl_array_release(fdo);
+  gkyl_array_release(fdo_ho);
   gkyl_array_release(ftar);
-  gkyl_array_release(fref);
+  gkyl_array_release(ftar_ho);
+  gkyl_array_release(fref_ho);
 }
 
-void test_2x_to_1x_p1(void) { test_2x_to_1x(1); }
-void test_2x_to_1x_p2(void) { test_2x_to_1x(2); }
+void test_dg_evproj_2x_to_1x_p1_ho(void) {
+  test_2x_to_1x(1, false);
+}
+
+void test_dg_evproj_2x_to_1x_p2_ho(void) {
+  test_2x_to_1x(2, false);
+}
 
 TEST_LIST = {
-  { "test_2x_to_1x_p1", test_2x_to_1x_p1 },
-  { "test_2x_to_1x_p2", test_2x_to_1x_p2 },
+  { "test_dg_evproj_2x_to_1x_p1_ho", test_dg_evproj_2x_to_1x_p1_ho },
+  { "test_dg_evproj_2x_to_1x_p2_ho", test_dg_evproj_2x_to_1x_p2_ho },
   { NULL, NULL },
 };
