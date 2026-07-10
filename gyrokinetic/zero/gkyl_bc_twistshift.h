718cd6348be400f1fcfb7a673bdb3b8622987ece
manauref <mfrancisquez@gmail.com>
2026-07-10 16:30:03 -0700

    Change signature of bc_twistshift_new so that structs are passed by address, this makes it easier to call it from python. Rename num_cell_ variables in tcv reg tests so that they use Nx like all other reg tests (makes it easier to automate changes to them).
    (cherry picked from commit ce4c1287a38415c1c06cd65e519368d8b95d9287)

--- gyrokinetic/zero/gkyl_bc_twistshift.h
+++ gyrokinetic/zero/gkyl_bc_twistshift.h
@@ -16,10 +16,10 @@ struct gkyl_bc_twistshift_inp {
   int shear_dir; // Direction in which the shift varies (shear).
   enum gkyl_edge_loc edge; // Edge of to apply this BC at (lower/upper).
   int cdim; // Configuration space dimensions.
-  struct gkyl_range bcdir_ext_update_r; // Local range where to apply BC, extended in bc_dir.
+  const struct gkyl_range *bcdir_ext_update_r; // Local range where to apply BC, extended in bc_dir.
   const int *num_ghost; // Number of ghost cells in each direction.
-  struct gkyl_basis basis; // Basis of the field shifted.
-  struct gkyl_rect_grid grid; // Grid the field shifted is defined on.
+  const struct gkyl_basis *basis; // Basis of the field shifted.
+  const struct gkyl_rect_grid *grid; // Grid the field shifted is defined on.
   evalf_t shift_func; // Function defining the shift.
   void *shift_func_ctx; // Context for shift_func.
   struct gkyl_array *shift_dg; // Discretized shift.
@@ -56,8 +56,8 @@ struct gkyl_bc_twistshift* gkyl_bc_twistshift_inew(const struct gkyl_bc_twistshi
  * @return New updater pointer.
  */
 struct gkyl_bc_twistshift* gkyl_bc_twistshift_new(int bc_dir, int shift_dir, int shear_dir,
-  enum gkyl_edge_loc edge, int cdim, struct gkyl_range bcdir_ext_update_r, const int *num_ghost,
-  struct gkyl_basis basis, struct gkyl_rect_grid grid, evalf_t shift_func, void *shift_func_ctx,
+  enum gkyl_edge_loc edge, int cdim, const struct gkyl_range *bcdir_ext_update_r, const int *num_ghost,
+  const struct gkyl_basis *basis, const struct gkyl_rect_grid *grid, evalf_t shift_func, void *shift_func_ctx,
   struct gkyl_array *shift_dg, int shift_poly_order, bool use_gpu);
 
 /**
