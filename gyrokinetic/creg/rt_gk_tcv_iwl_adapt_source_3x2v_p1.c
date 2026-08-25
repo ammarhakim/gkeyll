19c21dc38d925ca2591e31b2df01fa2e2a0e23d5
antoinehoff <antoine-hoffmann@hotmail.com>
2026-08-25 17:29:32 +0200

    fix typo in the cutoff wavelength

--- gyrokinetic/creg/rt_gk_tcv_iwl_adapt_source_3x2v_p1.c
+++ gyrokinetic/creg/rt_gk_tcv_iwl_adapt_source_3x2v_p1.c
@@ -832,7 +832,7 @@ main(int argc, char **argv)
     .parallel_upper_bc_shift_func = bc_shift_func_up,
     .parallel_lower_bc_shift_ctx = &ctx,
     .parallel_upper_bc_shift_ctx = &ctx,
-    .ts_filter_cutoff_wavelength = 2.0*ctx.Nx/ctx.Lx,
+    .ts_filter_cutoff_wavelength = 2.0*ctx.Lx/ctx.Nx,
     .ts_filter_half_width = 1,
     .ts_upsample_factor = 2,
   };
